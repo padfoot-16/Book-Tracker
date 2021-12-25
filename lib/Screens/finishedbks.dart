12bab17549dcd770f159d5123f63df5d8405fac1
padfoot-16 <amigo_awini@hotmail.com>
2021-12-25 03:59:27 +0100

    create and read from db works and mainpage listview works

---
+++ lib/Screens/finishedbks.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Finishedbks extends StatefulWidget {
+  const Finishedbks({ Key? key }) : super(key: key);
+
+  @override
+  _FinishedbksState createState() => _FinishedbksState();
+}
+
+class _FinishedbksState extends State<Finishedbks> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/Screens/mainpage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  CollectionReference books=FirebaseFirestore.instance.collection('Books');
+  TextEditingController _namecontroller=TextEditingController();
+  TextEditingController _pagecontroller=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(child:Text("Books:",style: TextStyle(fontSize: 20))),
+            Container(child:Text("Pages:",style: TextStyle(fontSize: 20))),
+          ],
+        ),
+        Expanded(
+          child: StreamBuilder(
+            stream: books.snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if(!snapshot.hasData){
+                return Center(child: Text("Loading..."),);
+              }
+              final data= snapshot.requireData;
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder:(context,index){
+                    return Padding(
+                      padding:const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: ListTile(
+                          title:Text("${data.docs[index]["name"]}"),
+                          trailing:Text("${data.docs[index]["page"]}") ,
+                        ),
+                      ), 
+                    );
+                  } 
+                );
+              
+            },
+          )
+          )
+      ],
+    );
+  }
+  
+
+  
+}
+
+class func{
+  void addScreen(){
+    
+  }
+}
